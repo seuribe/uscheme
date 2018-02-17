5a31583fe7319b3a2a73832cf642af24aebd952e
Sebastian Uribe <suribe@gmail.com>
2018-02-17 18:09:08 +0100

    - removed duplicated logic in Get & Find functions from Env

--- uscheme/Env.cs
+++ uscheme/Env.cs
@@ -2,52 +2,36 @@
 using System.Collections.Generic;
 
 namespace UScheme {
-    public class Env
-    {
+
+    public class Env {
         private Dictionary<string, Exp> values = new Dictionary<string, Exp>();
 
         private Env outer = null;
-        Procedure proc = null;
+
         public Env() { }
 
-        public static Env InitialEnv()
-        {
+        public static Env InitialEnv() {
             Env env = new Env();
             StdLib.AddProcedures(env);
-
             return env;
         }
 
-        public Env(Env outer)
-        {
+        public Env(Env outer) {
             this.outer = outer;
         }
 
-        public Env Find(string name)
-        {
+        public Env Find(string name) {
             if (values.ContainsKey(name))
-            {
                 return this;
-            }
+            
             if (outer != null)
-            {
                 return outer.Find(name);
-            }
+
             throw new Exception("symbol '" + name + "' not found");
         }
 
-        public Exp Get(string name)
-        {
-            Exp value;
-            if (values.TryGetValue(name, out value))
-            {
-                return value;
-            }
-            if (outer != null)
-            {
-                return outer.Get(name);
-            }
-            throw new Exception("symbol '" + name + "' not found");
+        public Exp Get(string name) {
+            return Find(name).values[name];
         }
 
         public void BindDefinitions(UList definitions) {
