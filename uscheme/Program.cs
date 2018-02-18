72b19a0f71a00e2951c7f08112e77627f4c87f5f
Sebastian Uribe <suribe@gmail.com>
2018-02-18 17:53:16 +0100

    - moved repl to its own class

--- uscheme/Program.cs
+++ uscheme/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace UScheme {
 
@@ -33,67 +29,9 @@ namespace UScheme {
                 UReader.Load(cmdOptions.LoadFilename, environment);
 
             if (cmdOptions.StartRepl || cmdOptions.LoadFilename == null)
-                Repl(Console.In, Console.Out, environment);
+                new REPL(Console.In, Console.Out, environment).Start();
             else
                 Console.In.ReadLine();
-        }
-
-        static void Repl(TextReader textIn, TextWriter textOut, Env environment) {
-            var buffer = new StringBuilder();
-            while (true) {
-                try {
-                    textOut.Write("uscheme > ");
-
-                    var line = textIn.ReadLine();
-                    if (line.Equals("!quit"))
-                        break;
-
-                    if (line.Equals("!buffer")) {
-                        textOut.WriteLine(buffer.ToString());
-                        continue;
-                    }
-
-                    if (line.Equals("!clear")) {
-                        buffer.Clear();
-                        continue;
-                    }
-
-                    buffer.Append(line);
-                    line = buffer.ToString();
-                    buffer.Append("\n");
-                    if (!IsProperForm(line))
-                        continue;
-
-
-                    using (var lineStream = new StringReader(line)) {
-                        var form = UReader.ReadForm(lineStream);
-                        var expression = UScheme.Eval(form, environment);
-                        textOut.WriteLine(expression.ToString());
-                    }
-                    buffer.Clear();
-                } catch (IOException) {
-                    break;
-                } catch (Exception e) {
-                    textOut.WriteLine("Error: " + e.Message);
-                    buffer.Clear();
-                }
-            }
-        }
-
-        static bool IsProperForm(string str) {
-            str.Trim();
-            var chars = str.ToCharArray();
-            int openParens = 0;
-
-            for (int i = 0 ; i < chars.Length && openParens >= 0; i++) {
-                if (chars[i] == '(')
-                    openParens++;
-                else if (chars[i] == ')')
-                    openParens--;
-            }
-
-            return openParens == 0 &&
-                (chars[0] == '(') == (chars[chars.Length - 1] == ')');
         }
     }
 }
---
+++ uscheme/REPL.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UScheme {
+    public class REPL {
+        readonly TextReader textIn;
+        readonly TextWriter textOut;
+        readonly Env environment;
+
+        bool exit = false;
+        StringBuilder buffer = new StringBuilder();
+
+        public REPL(TextReader textIn, TextWriter textOut, Env environment) {
+            this.textIn = textIn;
+            this.textOut = textOut;
+            this.environment = environment;
+        }
+
+        bool ProcessCommand(string line) {
+            if (line.Equals("!quit"))
+                exit = true;
+            else if (line.Equals("!buffer"))
+                textOut.WriteLine(buffer.ToString());
+            else if (line.Equals("!clear"))
+                buffer.Clear();
+            else
+                return false;
+
+            return true;
+        }
+
+        public void Start() {
+            while (!exit) {
+                textOut.Write("uscheme > ");
+
+                var line = textIn.ReadLine();
+                if (ProcessCommand(line))
+                    continue;
+
+                buffer.Append(line);
+
+                if (BufferParensMatch())
+                    ProcessBuffer();
+            }
+        }
+
+        void ProcessBuffer() {
+            try {
+                using (var lineStream = new StringReader(buffer.ToString())) {
+                    var form = UReader.ReadForm(lineStream);
+                    var expression = UScheme.Eval(form, environment);
+                    textOut.WriteLine(expression.ToString());
+                }
+            } catch (Exception e) {
+                textOut.WriteLine("Error: " + e.Message);
+            } finally {
+                buffer.Clear();
+            }
+        }
+
+        bool BufferParensMatch() {
+            var str = buffer.ToString();
+            var chars = str.ToCharArray();
+            int openParens = 0;
+
+            for (int i = 0 ; i < chars.Length && openParens >= 0 ; i++) {
+                if (chars[i] == '(')
+                    openParens++;
+                else if (chars[i] == ')')
+                    openParens--;
+            }
+
+            return openParens == 0 &&
+                (chars[0] == '(') == (chars[chars.Length - 1] == ')');
+        }
+    }
+}
