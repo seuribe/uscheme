03ae9bd1b5a775b1473084cf30d12be684f16bae
Sebastian Uribe <suribe@gmail.com>
2018-02-21 21:31:31 +0100

    - more test cases for parsing and tokenizing

--- uscheme-tests/Tests/TestParser.cs
+++ uscheme-tests/Tests/TestParser.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace UScheme.Tests {
     [TestFixture]
     public class TestParser {
+
+        static readonly string SimpleSum = "(+ 1 2)";
+        static readonly List<string> SimpleSumTokens = new List<string> { "(", "+", "1", "2", ")"};
+        static readonly string NestedSum = "(+ 3 (+ 1 2) 4)";
+        static readonly List<string> NestedSumTokens = new List<string> { "(", "+", "3", "(", "+", "1", "2", ")", "4", ")" };
 
         [TestCase("(", " ( ")]
         [TestCase(")", " ) ")]
@@ -16,12 +22,30 @@ namespace UScheme.Tests {
             Assert.AreEqual(expected, output);
         }
 
-
         [TestCase("(;", " ( ")]
         [TestCase("(; fdjfdskf ds fds flds", " ( ")]
         public void PreProcessRemovesComments(string input, string expected) {
             var output = UReader.PreProcessInput(input);
             Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TokenizeLeavesAtomAlone() {
+            var output = UReader.Tokenize("atom");
+            Assert.AreEqual(1, output.Count);
+            Assert.AreEqual("atom", output[0]);
+        }
+
+        [Test]
+        public void TokenizeSimpleForm() {
+            var output = UReader.Tokenize(SimpleSum);
+            CollectionAssert.AreEqual(SimpleSumTokens, output);
+        }
+
+        [Test]
+        public void TokenizeNestedForm() {
+            var output = UReader.Tokenize(NestedSum);
+            CollectionAssert.AreEqual(NestedSumTokens, output);
         }
     }
 }
