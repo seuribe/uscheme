seuribe/uscheme

commit 7b30ff0b66e135381e382d00fc28e41fa2562d3e

- replaced UList with Cells (some method implementations are very inefficient and ugly, probably need cleaning up) - added more test cases

uscheme/StdLib.cs
@@ -1,22 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace UScheme {
     partial class StdLib {
 
-        public static void EnsureArity(UList list, int size) {
-            if (list.Count != size)
-                throw new EvalException("procedure accepts only " + size + " arguments, " + list.Count + " provided");
+        public static void EnsureArity(Cell list, int size) {
+            var length = list.Length();
+            if (length != size)
+                throw new EvalException("procedure accepts only " + size + " arguments, " + length + " provided");
         }
 
-        public static void EnsureArityMin(UList list, int size) {
-            if (list.Count < size)
-                throw new EvalException("procedure accepts only " + size + " arguments, " + list.Count + " provided");
+        public static void EnsureArityMin(Cell list, int size) {
+            var length = list.Length();
+            if (length != size)
+                throw new EvalException("procedure accepts only " + size + " arguments, " + length + " provided");
+        }
+
+        public static void EnsureAll(Cell list, Func<Exp, bool> predicate, string error) {
+            foreach (var exp in list.Iterate())
+                if (!predicate(exp))
+                    throw new EvalException(error);
+        }
+
+        public static void EnsureIs<T>(Exp exp) {
+            if (!(exp is T))
+                throw new EvalException("Expected " + typeof(T).ToString() + ", got " + typeof(Exp).ToString());
         }
 
         private static Procedure BuildIsProcedure<T>(Func<T, bool> evalFunction = null) where T : Exp {
-            return new Procedure((UList parameters, Env env) => {
+            return new Procedure((Cell parameters, Env env) => {
                 EnsureArity(parameters, 1);
                 evalFunction = evalFunction ?? (e => true);
                 return Boolean.Get(parameters.First is T && evalFunction((T)parameters.First));
@@ -29,120 +43,123 @@ namespace UScheme {
         private static readonly Procedure IsProcedure = BuildIsProcedure<Procedure>();
         private static readonly Procedure IsSymbol = BuildIsProcedure<Symbol>();
         private static readonly Procedure IsBoolean = BuildIsProcedure<Boolean>();
-        private static readonly Procedure IsList = BuildIsProcedure<UList>();
+        private static readonly Procedure IsList = BuildIsProcedure<Cell>();
 
-        private static readonly Procedure List = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure List = new Procedure((Cell parameters, Env env) => {
             return parameters;
         });
 
-        private static readonly Procedure Equal = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Equal = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 2);
             return Boolean.Get(parameters.First.UEquals(parameters.Second));
         });
 
-        private static readonly Procedure Not = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Eq = new Procedure((Cell parameters, Env env) => {
+            EnsureArity(parameters, 2);
+            return Boolean.Get(parameters.First == parameters.Second);
+        });
+
+        private static readonly Procedure Not = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 1);
             return (parameters.First == Boolean.FALSE) ? Boolean.TRUE : Boolean.FALSE;
         });
 
-        private static readonly Procedure Car = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Car = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 1);
-            var pair = parameters.First as UList;
-            return pair.First;
+            var pair = parameters.First as Cell;
+            return pair.car;
         });
 
-        private static readonly Procedure Cdr = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Cdr = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 1);
-            var pair = parameters.First as UList;
-            return pair.Second;
+            var pair = parameters.First as Cell;
+            return pair.cdr;
         });
 
-        private static readonly Procedure Cons = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Cons = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 2);
-            return new UList { parameters.First, parameters.Second };
+            return new Cell(parameters.First, parameters.Second);
         });
 
-        private static readonly Procedure Length = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Length = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 1);
-            return new IntegerNumber((parameters.First as UList).Count);
+            return new IntegerNumber((parameters.First as Cell).Length());
         });
 
-        private static readonly Procedure Append = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Append = new Procedure((Cell parameters, Env env) => {
             EnsureArityMin(parameters, 1);
+            EnsureAll(parameters, exp => (exp is Cell) && (exp as Cell).IsList, "append parameters must be lists");
 
-            var newList = new UList();
-            for (int i = 0 ; i < parameters.Count; i++) {
-                var sublist = parameters[i] as UList;
-                if (sublist == null)
-                    throw new EvalException("append parameter " + (i + 1) + " is not a list");
+            var cell = Cell.Duplicate(parameters.First as Cell);
+            foreach (Cell sublist in parameters.Rest().Iterate())
+                cell = Cell.Append(cell, sublist);
 
-                newList.AddRange(sublist);
-            }
-            return newList;
+            return cell;
         });
 
-        private static readonly Procedure Apply = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Apply = new Procedure((Cell parameters, Env env) => {
             EnsureArityMin(parameters, 2);
-            var procedure = parameters.First as Procedure;
-            var listParameter = parameters.Second as UList;
+            EnsureIs<Procedure>(parameters.First);
+            EnsureIs<Cell>(parameters.LastCell());
 
-            if (parameters.Count > 2) {
-                UList firstArgs = new UList();
-                foreach (Exp e in parameters.GetRange(1, parameters.Count - 2)) {
-                    firstArgs.Add(e);
-                }
-                firstArgs.AddRange(listParameter);
-                listParameter = firstArgs;
-            }
-            return procedure.Eval(listParameter);
+            var procedure = parameters.First as Procedure;
+            if (parameters.Length() > 2)
+                throw new UException("Not implemented: apply cannot accept more than the procedure and a list");
+
+            return procedure.Eval(parameters.Rest());
         });
 
         private static readonly Procedure Map = new Procedure((parameters, env) => {
             var proc = parameters.First as Procedure;
-            var args = parameters.Tail();
-            return new UList(args.Select(arg => proc.Eval(arg)));
+            var args = parameters.Rest();
+
+            var list = new List<Exp>();
+            foreach (var exp in parameters.Iterate())
+                list.Add(proc.Eval(exp));
+
+            return Cell.BuildList(list);
         });
 
-        public static Exp FoldlBase(Procedure op, UList parameters, Env env) {
+        public static Exp FoldlBase(Procedure op, Cell parameters, Env env) {
             EnsureArityMin(parameters, 2);
             var value = parameters.First;
 
-            for (int i = 1 ; i < parameters.Count ; i++)
-                value = op.Eval(new UList() { value, parameters[i] });
+            for (int i = 1 ; i < parameters.Length() ; i++)
+                value = op.Eval(Cell.BuildList(value, parameters[i]));
 
             return value;
         }
 
-        private static readonly Procedure Foldl = new Procedure((parameters, env) => {
+        private static readonly Procedure Foldl = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 3);
             var op = parameters.First as Procedure;
             var value = parameters.Second;
-            var list = parameters.Third as UList;
-            foreach (var e in list)
-                value = op.Eval(new UList() { value, e });
+            var list = parameters.Third as Cell;
+            foreach (var e in list.Iterate())
+                value = op.Eval(Cell.BuildList(value, e));
             
             return value;
         });
 
-        private static readonly Procedure Print = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Print = new Procedure((Cell parameters, Env env) => {
             var ev = parameters.First;
             // TODO: output should be configurable somewhere
             Console.Out.WriteLine(ev.ToString());
             return ev;
         });
 
-        private static readonly Procedure StringAppend = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure StringAppend = new Procedure((Cell parameters, Env env) => {
             var sb = new StringBuilder();
-            foreach (Exp substring in parameters)
+            foreach (Exp substring in parameters.Iterate())
                 sb.Append((substring as UString).str);
             
             return new UString(sb.ToString());
         });
 
-        private static readonly Procedure Nth = new Procedure((UList parameters, Env env) => {
+        private static readonly Procedure Nth = new Procedure((Cell parameters, Env env) => {
             EnsureArity(parameters, 2);
             var index = parameters.First as IntegerNumber;
-            var list = parameters.Second as UList;
+            var list = parameters.Second as Cell;
             return list[index.IntValue];
         });
 
@@ -174,7 +191,8 @@ namespace UScheme {
             env.Bind("procedure?", IsProcedure);
             env.Bind("symbol?", IsSymbol);
             env.Bind("list?", IsList);
-            env.Bind("eq?", Equal);
+            env.Bind("equal?", Equal);
+            env.Bind("eq?", Eq);
 
             env.Bind("print", Print);
 

uscheme-tests/Tests/TestCell.cs
@@ -6,10 +6,13 @@ namespace UScheme.Tests {
     [TestFixture]
     public class TestCell : TestConstants {
 
+        static Cell CList;
+        static Cell ABACList;
         static Cell ABCell;
         static Cell CABCell;
         static Cell ABCCellFromList;
         static Cell ABCCellFromArray;
+        static Cell BCCellFromList;
 
         [SetUp]
         public void SetUp() {
@@ -17,6 +20,9 @@ namespace UScheme.Tests {
             CABCell = new Cell(SymbolC, ABCell);
             ABCCellFromList = Cell.BuildList(new List<Exp> { SymbolA, SymbolB, SymbolC });
             ABCCellFromArray = Cell.BuildList(SymbolA, SymbolB, SymbolC);
+            BCCellFromList = Cell.BuildList(SymbolB, SymbolC);
+            CList = Cell.BuildList(SymbolC);
+            ABACList = Cell.BuildList(SymbolA, SymbolB, SymbolA, SymbolC);
         }
 
         [Test]
@@ -67,6 +73,33 @@ namespace UScheme.Tests {
         }
 
         [Test]
+        public void BuildsUnitaryList() {
+            var list = Cell.BuildList(SymbolC);
+            Assert.AreEqual(1, list.Length());
+            Assert.IsTrue(list.IsList);
+            Assert.IsFalse(list.IsNull);
+            Assert.IsTrue(SymbolC.UEquals(list.First));
+        }
+
+        [Test]
+        public void PositionFunctions() {
+            var list = Cell.BuildList(SymbolA, SymbolB, SymbolA, SymbolC);
+
+            Assert.IsTrue(SymbolA.Equals(list.First));
+            Assert.IsTrue(SymbolB.Equals(list.Second));
+            Assert.IsTrue(SymbolA.Equals(list.Third));
+            Assert.IsTrue(SymbolC.Equals(list.Fourth));
+        }
+
+        [Test]
+        public void BuildsEmptyListIfNoElements() {
+            var list = Cell.BuildList();
+            Assert.IsTrue(list.IsNull);
+            Assert.IsTrue(list.IsList);
+            Assert.AreEqual(0, list.Length());
+        }
+
+        [Test]
         public void BuildFromEmptyListIsNull() {
             Assert.IsTrue(Cell.BuildList(new List<Exp>()).IsList);
             Assert.IsTrue(Cell.BuildList().IsList);
@@ -91,6 +124,68 @@ namespace UScheme.Tests {
         [Test]
         public void NthGetsCell() {
             Assert.AreEqual(SymbolA, ABCCellFromList[0]);
+        }
+
+        [Test]
+        public void IteratesOvelCellList() {
+            var values = new List<Exp>();
+            foreach (var exp in ABCCellFromList.Iterate()) {
+                values.Add(exp);
+            }
+            Assert.AreEqual(new List<Exp> { SymbolA, SymbolB, SymbolC }, values);
+        }
+
+        [Test]
+        public void LastReturnsLast() {
+            Assert.IsTrue(SymbolC.UEquals(ABACList.Last()));
+        }
+
+        [Test]
+        public void LastCellReturnsLastCell() {
+            var lastCell = ABCCellFromList.LastCell();
+            Assert.AreEqual(SymbolC, lastCell.car);
+            Assert.AreEqual(Cell.Null, lastCell.cdr);
+        }
+
+        [Test]
+        public void RestReturnsRest() {
+            Assert.IsTrue(BCCellFromList.UEquals(ABCCellFromList.Rest()));
+        }
+
+        [Test]
+        public void CannotAddToNullList() {
+            var list = Cell.Null;
+            Assert.Throws<UException>(() => list.Add(SymbolC));
+        }
+
+        [Test]
+        public void DuplicateDuplicates() {
+            var dup = Cell.Duplicate(ABACList);
+            Assert.IsTrue(dup.UEquals(ABACList));
+        }
+
+        [Test]
+        public void DuplicateDoesNotModifyOriginal() {
+            var dup = Cell.Duplicate(ABACList);
+            dup.car = SymbolC;
+            Assert.IsFalse(dup.UEquals(ABACList));
+            Assert.AreEqual(SymbolC, dup.car);
+            Assert.AreEqual(SymbolA, ABACList.car);
+        }
+
+        [Test]
+        public void AppendAppends() {
+            var list = Cell.Append(ABACList, CList);
+            var expected = Cell.BuildList(SymbolA, SymbolB, SymbolA, SymbolC, SymbolC);
+            Assert.IsTrue(expected.UEquals(list));
+        }
+
+        [Test]
+        public void AppendDoesNotModifyOriginal() {
+            var list = Cell.Append(ABACList, CList);
+            var expected = Cell.BuildList(SymbolA, SymbolB, SymbolA, SymbolC, SymbolC);
+            list.car = SymbolB;
+            Assert.AreEqual(SymbolA, ABACList.car);
         }
     }
 }

uscheme-tests/Tests/TestConstants.cs
@@ -13,11 +13,9 @@ namespace UScheme.Tests {
         protected static readonly IntegerNumber Number3 = new IntegerNumber(3);
         protected static readonly IntegerNumber Number4 = new IntegerNumber(4);
 
-        protected static readonly UList SimpleSumForm = new UList { PlusSymbol, Number1, Number2 };
-        protected static readonly UList NestedSumForm = new UList {
-            PlusSymbol, Number3, new UList {
-                        PlusSymbol, Number1, Number2 },
-            Number4 };
+        protected static readonly Cell SimpleSumForm = Cell.BuildList(PlusSymbol, Number1, Number2);
+        protected static readonly Cell NestedSumForm = Cell.BuildList(
+            PlusSymbol, Number3, Cell.BuildList(PlusSymbol, Number1, Number2), Number4);
 
 
         protected static readonly Symbol SymbolA = Symbol.From("a");

uscheme-tests/Tests/TestParser.cs
@@ -13,7 +13,7 @@ namespace UScheme.Tests {
         [Test]
         public void ParseSingletonList() {
             var expression = Parser.Parse("(f)");
-            Assert.IsTrue(expression.UEquals(new UList { Symbol.From("f") }));
+            Assert.IsTrue(expression.UEquals(Cell.BuildList(Symbol.From("f"))));
         }
 
         [Test]
@@ -31,7 +31,7 @@ namespace UScheme.Tests {
         [Test]
         public void ParseQuote() {
             var expression = Parser.Parse("'a");
-            Assert.IsTrue(expression.UEquals(new UList { Symbol.QUOTE, Symbol.From("a") }));
+            Assert.IsTrue(expression.UEquals(Cell.BuildList(Symbol.QUOTE, Symbol.From("a"))));
         }
 
         [TestCase("a")]

uscheme/Env.cs
@@ -34,8 +34,8 @@ namespace UScheme {
             return Find(name).values[name];
         }
 
-        public void BindDefinitions(UList definitions) {
-            foreach (UList def in definitions)
+        public void BindDefinitions(Cell definitions) {
+            foreach (Cell def in definitions.Iterate())
                 Bind(def.First.ToString(), UScheme.Eval(def.Second, this));
         }
 

uscheme/Exception/UException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace UScheme {
-    class UException : Exception {
+    public class UException : Exception {
         public UException(string message) : base(message) { }
     }
 }

uscheme/Parser.cs
@@ -42,17 +42,17 @@ namespace UScheme {
             var token = tokens.Current;
             tokens.MoveNext();
             if (token == "(") {
-                var list = new UList();
+                var list = new List<Exp>();
                 while (tokens.Current != ")") {
                     list.Add(ReadFromTokens(tokens));
                 }
                 tokens.MoveNext();
-                return list;
+                return Cell.BuildList(list);
             }
             if (token == ")")
                 throw new ParseException("Misplaced ')'");
             if (token == "'")
-                return new UList { Symbol.QUOTE, ReadFromTokens(tokens) };
+                return Cell.BuildList(Symbol.QUOTE, ReadFromTokens(tokens));
 
             return Atom(token);
         }

uscheme/Types/Cell.cs
@@ -11,6 +11,7 @@ namespace UScheme {
 
         public bool IsNull => this == Null;
         public bool IsList => IsNull || ((cdr is Cell) && (cdr as Cell).IsList);
+        public bool IsPair => this != Null;
 
         public static readonly Cell Null = new Cell();
 
@@ -21,21 +22,95 @@ namespace UScheme {
 
         public Exp this[int index] {
             get {
-                if (!IsList)
-                    throw new UException("Cell not list!");
+                EnsureIsList();
                 return NthCell(index).car;
             }
             set {
-                if (!IsList)
-                    throw new UException("Cell not list!");
+                EnsureIsList();
                 NthCell(index).car = value;
             }
         }
 
-        Cell() { }
+        protected Cell() { }
         public Cell(Exp car, Exp cdr) {
             this.car = car;
             this.cdr = cdr;
+        }
+
+        void EnsureIsList() {
+            if (!IsList)
+                throw new UException("Cell not a list: " + ToString());
+        }
+        
+        void EnsureNotNull() {
+            if (IsNull)
+                throw new UException("Cell must be non-null");
+        }
+
+        public void Add(Exp exp) {
+            EnsureNotNull();
+            LastCell().cdr = new Cell(exp, Null);
+        }
+
+        public static Cell Duplicate(Cell other) {
+            if (other.IsNull)
+                return Null;
+
+            if (!other.IsList)
+                return new Cell(other.car, other.cdr);
+
+            return DoDuplicate(other);
+        }
+
+        static Cell DoDuplicate(Cell other) {
+            if (other == Null)
+                return Null;
+            return new Cell(other.car, DoDuplicate(other.cdr as Cell));
+        }
+
+        public void RemoveLast() {
+            throw new Exception("not implemented");
+        }
+
+        public static Cell Append(Cell first, Cell second) {
+            first.EnsureIsList();
+            second.EnsureIsList();
+            var list = Duplicate(first);
+            list.LastCell().cdr = Duplicate(second);
+            return list;
+        }
+
+        public Exp Last() {
+            return LastCell().car;
+        }
+
+        public Cell LastCell() {
+            EnsureIsList();
+            var cell = this;
+            while (cell.cdr != Null)
+                cell = cell.cdr as Cell;
+
+            return cell;
+        }
+
+        public Cell Rest() {
+            EnsureIsList();
+            return cdr as Cell;
+        }
+
+        public int Length() {
+            EnsureIsList();
+            return IsNull ? 0 : (1 + Rest().Length());
+        }
+
+        public IEnumerable<Exp> Iterate() {
+            EnsureIsList();
+
+            var cell = this;
+            while (cell != null && !cell.IsNull) {
+                yield return cell.car;
+                cell = cell.cdr as Cell;
+            }
         }
 
         Cell NthCell(int index) {
@@ -62,6 +137,13 @@ namespace UScheme {
                 return ToListString();
 
             return CharConstants.ParensOpen + car.ToString() + " . " + cdr.ToString() + CharConstants.ParensClose;
+        }
+
+        public List<string> ToStringList() {
+            var strings = new List<string>();
+            foreach (var exp in Iterate())
+                strings.Add(exp.ToString());
+            return strings;
         }
 
         string ToListString() {

commit 7e6eb323b43933983c560213eb09e21f49b493c7

- fixed bug introduced in 7b30ff0b66e135381e382d00fc28e41fa2562d3e , EnsureArityMin now again checks min, not exact arity

uscheme/StdLib.cs
@@ -14,8 +14,8 @@ namespace UScheme {
 
         public static void EnsureArityMin(Cell list, int size) {
             var length = list.Length();
-            if (length != size)
-                throw new EvalException("procedure accepts only " + size + " arguments, " + length + " provided");
+            if (length < size)
+                throw new EvalException("procedure needs at least " + size + " arguments, " + length + " provided");
         }
 
         public static void EnsureAll(Cell list, Func<Exp, bool> predicate, string error) {