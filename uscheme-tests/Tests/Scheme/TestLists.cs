7b4d3c062f6d2a63e5b91d175cf21eb1f7bf591f
Sebastian Uribe <suribe@gmail.com>
2018-03-06 15:35:58 +0100

    - added test cases for nth

---
+++ uscheme-tests/Tests/Scheme/TestLists.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace UScheme.Tests {
+    [TestFixture]
+    public class TestLists : TestEval {
+        [TestCase("'(1 2 3 4)", 3, "4")]
+        [TestCase("'(1 2 3 4 5 6)", 0, "1")]
+        public void Nth(string listExpression, int index, string expected) {
+            WhenEvaluating("(nth " + index + " " + listExpression + ")");
+            ThenResultIsExp(Parser.Parse(expected));
+        }
+    }
+}
