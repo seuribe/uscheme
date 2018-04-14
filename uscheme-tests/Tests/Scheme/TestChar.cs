1302df3308de4076f53b8fc242e1087e5c0660b2
Sebastian Uribe <suribe@gmail.com>
2018-04-14 18:45:17 +0100

    - added test cases for char->integer

---
+++ uscheme-tests/Tests/Scheme/TestChar.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UScheme.Tests {
+    [TestFixture]
+    public class TestChar : TestEval {
+
+        [TestCase(@"#\t", 116)]
+        [TestCase(@"#\a", 97)]
+        public void CharToInteger(string ch, int value) {
+            WhenEvaluating("(char->integer " + ch + ")");
+            ThenIntegerResultIs(value);
+        }
+    }
+}
